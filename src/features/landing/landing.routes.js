ccba5bc9ba756e7fa8f8da334ac9bb49a62a0d29
Auldey Glen <auldeyjerusalem2@gmail.com>
2026-08-16 15:32:12 +0800

    refactor: redesign navbar and reorganize feature static files

--- src/features/landing/landing.routes.js
+++ src/features/landing/landing.routes.js
@@ -6,4 +6,13 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'landing.html'));
 });
 
+// STATIC assets
+router.get('/landing.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'landing.css'));
+});
+
+router.get('/landing.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'landing.js'));
+});
+
 module.exports = router;
